b96dc5eda14c664f03590e27d1307b53a3298670
mmachida <mmachida@cs.washington.edu>
2025-12-17 15:50:48 -0800

    add new pages and updated navbar to have left and right side

---
+++ src/app/designs/page.tsx
@@ -0,0 +1,12 @@
+import NavBar from "@/components/NavBar";
+
+export default function Designs() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <main className="flex min-h-screen w-full flex flex-col">
+        <NavBar/>
+        designs page
+      </main>
+    </div>
+  );
+}
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,12 +4,18 @@ import Image from "next/image";
 export default function NavBar() {
   return (
     <div className="bg-green-900 w-full h-[10vh] px-16 py-8">
-      <nav className="w-full h-full gap-16 flex flex-row items-center">
-        <Link href="/..">
-          <Image src={""} alt={""}/>
-        </Link>
-        <Link href="/about">About</Link>
-      </nav>
+      <div className="w-full h-full flex flex-row justify-between">
+        <nav className="h-full gap-16 flex flex-row items-center">
+          <Link href="/..">
+            <Image src="/images/test.png" alt="Logo" width={100} height={100}/>
+          </Link>
+          <Link href="/about">About</Link>
+          <Link href="/designs">Designs</Link>
+        </nav>
+        <nav className="h-full flex flex-row items-center">
+          <Link href="/checkout">Cart</Link>
+        </nav>
+      </div>
     </div>
   );
 }
