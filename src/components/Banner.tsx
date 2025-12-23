e6235c6fc7c97781341a035e4ec526ed1ffbdcd8
mmachida <mmachida@cs.washington.edu>
2025-12-22 22:50:31 -0800

    add banner component

---
+++ src/components/Banner.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+type BannerProps = {
+  height: number
+  img: ImageProps
+  title?: string
+  subtitle?: string
+}
+
+export type ImageProps = {
+  src: string
+  alt: string
+}
+
+export default function Banner(props: BannerProps) {
+  return (
+    <div className="relative w-full h-full" style={{ height: `${props.height}vh` }}>
+      <Image src={props.img.src} alt={props.img.alt} fill className="object-cover"/>
+
+      {/* Dark overlay */}
+      <div className="absolute inset-0 bg-black/30 z-10 pointer-events-none"></div>
+
+      {/* Text overlay */}
+      {props.title == null? <div/> :
+        <div className="absolute bottom-0 left-0 p-20 z-20">
+          <h1 className="text-white">{props.title}</h1>
+          { props.subtitle == null? <div/> : <h3 className="text-white">{props.subtitle}</h3> }
+        </div>
+      }
+    </div>
+  );
+}
