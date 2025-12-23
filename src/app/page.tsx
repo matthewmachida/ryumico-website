9db5ce6ba7e6d62c2b4bde9b6917a9bb8070d9b9
mmachida <mmachida@cs.washington.edu>
2025-12-22 22:50:53 -0800

    update photo carousel to use banner images instead of passing image tags in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,22 @@ import PhotoCarousel from "@/components/PhotoCarousel";
 import NavBar from "@/components/NavBar";
 import PhotoAndText from "@/components/PhotoAndText";
 import Spacer from "@/components/Spacer";
-import Image from "next/image";
 import Footer from "@/components/Footer";
 
 export default function Home() {
   const list = [
-    <Image src="/images/c1.jpg" alt="carousel 1" fill className="object-cover"/>,
-    <Image src="/images/c2.jpeg" alt="carousel 2" fill className="object-cover"/>,
-    <Image src="/images/c3.JPG" alt="carousel 3" fill className="object-cover"/>
+    {
+      src: "/images/c1.jpg",
+      alt:"carousel 1"
+    },
+    {
+      src: "/images/c2.jpeg",
+      alt: "carousel 2"
+    },
+    {
+      src: "/images/c3.JPG",
+      alt: "carousel 3",
+    }
   ];
 
   return (
--- src/components/PhotoCarousel.tsx
+++ src/components/PhotoCarousel.tsx
@@ -5,10 +5,11 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css/autoplay";
 import "swiper/css";
+import Banner, { ImageProps } from "./Banner";
 
 type PhotoCarouselProps = {
   height: number // in vh units, not px!
-  slides: ReactElement[]
+  slides: ImageProps[]
   title?: string
   subtitle?: string
 };
@@ -21,29 +22,25 @@ export default function PhotoCarousel(props: PhotoCarouselProps) {
       <Swiper 
         className="w-full h-full"
         modules={[Autoplay]}
-        autoplay={{delay: 3000}}
+        autoplay={{delay: 4000}}
         loop={true}
         slidesPerView={1}
       >
         {props.slides.map((item, i) => (
           <SwiperSlide key={i}>
-            <div className="object-cover w-full h-full" style={{ height: `${props.height}vh` }}>
-              {item}
-            </div>
+            <Banner
+              height={props.height}
+              img={item}
+            />
           </SwiperSlide>
         ))}
       </Swiper>
-
-      {/* Dark overlay */}
-      <div className="absolute inset-0 bg-black/30 z-10 pointer-events-none"></div>
 
       {/* Text overlay */}
       {props.title == null? <div/> :
         <div className="absolute bottom-0 left-0 p-20 z-20">
           <h1 className="text-white">{props.title}</h1>
-          {props.subtitle == null? <div/> : 
-            <h3 className="text-white">{props.subtitle}</h3>
-          }
+          { props.subtitle == null? <div/> : <h3 className="text-white">{props.subtitle}</h3> }
         </div>
       }
     </div>
