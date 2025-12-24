0701cfe17c9dae963aa831a42487117abcd04368
mmachida <mmachida@cs.washington.edu>
2025-12-23 17:23:46 -0800

    add navigation arrows and pagination dots to item carousel

--- src/components/ItemCarousel.tsx
+++ src/components/ItemCarousel.tsx
@@ -1,8 +1,12 @@
 "use client"
 
+import Image from "next/image";
 import { ReactElement } from "react";
-import { Navigation } from "swiper/modules";
+import { Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
 
 type ItemCarousel = {
   items: ReactElement[]
@@ -11,38 +15,62 @@ type ItemCarousel = {
 export default function ItemCarousel(props: ItemCarousel) {
   return (
     <div className="relative w-full h-[55vh] px-8">
-      <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-full h-[5vh] flex flex-row justify-center py-2 gap-10">
-        <button className="prev-btn bg-purple-300 z-10">
-          previous
+      <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-full h-[4vh] flex flex-row items-center justify-center gap-8">
+        <button className="prev-btn w-[5vh] h-[5vh] z-10 flex flex-col items-center justify-center rounded-full">
+          <div className="relative w-[3vh] h-[3vh]">
+            <Image
+              src="/icons/arrow.svg"
+              alt="Right Arrow"
+              fill
+              className="object-cover [transform:scaleX(-1)]"
+            />
+          </div>
         </button>
-        <button className="next-btn bg-purple-300 z-10">
-          next
+        <button className="next-btn w-[5vh] h-[5vh] z-10 flex flex-col items-center justify-center rounded-full">
+          <div className="relative w-[3vh] h-[3vh]">
+            <Image
+              src="/icons/arrow.svg"
+              alt="Right Arrow"
+              fill
+              className="object-cover"
+            />
+          </div>
         </button>
       </div>
       <Swiper
-        modules={[Navigation]}
+        modules={[Navigation, Pagination]}
         navigation={{
           nextEl: ".next-btn",
           prevEl: ".prev-btn"
         }}
+        pagination={{
+          clickable: true
+        }}
         loop={false}
         slidesPerView={3}
         spaceBetween={20}
+        className="w-full h-[50vh]"
       >
         <SwiperSlide>
-          <div className="w-full h-[50vh] bg-red-600"/>
+          <div className="w-full h-[45vh] bg-red-600"/>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="w-full h-[50vh] bg-orange-600"/>
+          <div className="w-full h-[45vh] bg-orange-600"/>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="w-full h-[50vh] bg-yellow-600"/>
+          <div className="w-full h-[45vh] bg-yellow-600"/>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="w-full h-[50vh] bg-green-600"/>
+          <div className="w-full h-[45vh] bg-green-600"/>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="w-full h-[50vh] bg-blue-600"/>
+          <div className="w-full h-[45vh] bg-blue-600"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="w-full h-[45vh] bg-purple-600"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="w-full h-[45vh] bg-pink-600"/>
         </SwiperSlide>
       </Swiper>
     </div>
