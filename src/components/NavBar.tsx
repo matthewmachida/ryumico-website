3c52968843a5b7d1dfee15f65ea124502a3f7432
matthewmachida <mchida57@gmail.com>
2025-12-26 01:55:50 -0800

    update photo carousel to allow images and add new logos to home page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,13 +3,21 @@ import Image from "next/image";
 
 export default function NavBar() {
   return (
-    <div className="bg-white-900 w-full h-[10vh]">
+    <div className="bg-white-900 w-full h-24">
       <div className="w-full h-full px-16 flex flex-row justify-between">
         {/* Left Side of NavBar */}
         <nav className="h-full gap-16 flex flex-row items-center">
           <Link className="h-full" href="/..">
             {/* Logo image instead of text for the home page button */}
-            <Image className="nav-button" src="/images/test.png" alt="Logo" width={100} height={100}/>
+            <div className="relative h-full">
+              <Image
+                src="/logos/whale-logo.png"
+                alt="Logo"
+                width={0}
+                height={0}
+                sizes="150px"
+                className="nav-button h-24 w-auto"/>
+            </div>
           </Link>
           <Link className="h-full" href="/about">
             <h3 className="nav-button h-full px-8 flex flex-row items-center">About</h3>
--- src/components/PhotoCarousel.tsx
+++ src/components/PhotoCarousel.tsx
@@ -1,20 +1,35 @@
 "use client"
 
-import { ReactElement } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css/autoplay";
 import "swiper/css";
 import Banner, { ImageProps } from "./Banner";
+import Image from "next/image";
+import { error } from "console";
 
 type PhotoCarouselProps = {
   slides: ImageProps[]
   height: number // in vh units, not px!
   title?: string
   subtitle?: string
+  titleSrc?: string
+  titleAlt?: string
 };
 
+// There are two optional features - title text and title image
+// Both the titleSrc and titleAlt must be given for the title image to display, and either the combo of titleSrc and titleAlt or the title must be given to display the subtitle
+// If both are provided, the title image takes priority and title text is not displayed
 export default function PhotoCarousel(props: PhotoCarouselProps) {
+  if (props.titleSrc == undefined && props.titleAlt != undefined) {
+    console.error("No src for photo carousel title image provided");
+    return <div/>;
+  }
+  if (props.titleSrc != undefined && props.titleAlt == undefined) {
+    console.error("No alt for photo carousel title image provided");
+    return <div/>;
+  }
+  
   return(
     // Could work on making the features customizable based on props
     <div className="relative w-full" style={{ height: `${props.height}vh` }}>
@@ -36,12 +51,25 @@ export default function PhotoCarousel(props: PhotoCarouselProps) {
         ))}
       </Swiper>
 
-      {/* Text overlay */}
-      {props.title == null? <div/> :
-        <div className="absolute bottom-0 left-0 p-20 z-20">
-          <h1 className="text-white">{props.title}</h1>
-          { props.subtitle == null? <div/> : <h3 className="text-white">{props.subtitle}</h3> }
-        </div>
+      {/* Text/Photo overlay */}
+      {
+        props.title == undefined && (props.titleSrc == undefined || props.titleAlt == undefined)? // If we do not have title or a valid image
+          <div/> : // then display nothing
+          <div className="absolute bottom-0 left-0 z-20 w-full h-[30vh]">
+            {
+              (props.titleSrc != undefined && props.titleAlt != undefined)? // If have and img titleSrc and image titleAlt}
+                <Image
+                  src={props.titleSrc}
+                  alt={props.titleAlt}
+                  width={4}
+                  height={1}
+                  sizes={"850px"}
+                  className="absolute bottom-32 left-28 z-20 h-full w-auto object-cover"
+                /> : // then use the image
+                <h1 className="absolute bottom-32 left-40 z-20 text-white">{props.title}</h1> // else, use the text
+            }
+            { props.subtitle == null? <div/> : <h3 className="absolute bottom-20 left-40 z-20 text-white">{props.subtitle}</h3> }
+          </div>
       }
     </div>
     
