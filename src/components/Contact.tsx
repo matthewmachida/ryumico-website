8188c2a14bb9bc05577f4447568d40836378af11
matthewmachida <mchida57@gmail.com>
2025-12-29 02:08:02 -0800

    update email logo to use base path

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import PhotoAndText, { Color } from "./PhotoAndText";
 
+const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
+
 export default function Contact() {
   // TODO: make custom animation for email logo?
   return (
     <PhotoAndText
-      src="/icons/email.svg"
+      src={`${BASE_PATH}/icons/email.svg`}
       alt="Email icon"
       header="Need to Contact Us?"
       text="If you have any questions or inquiries, send us an email at example@email.com and we will try get back to you!"
